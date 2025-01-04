603d58f7d81587636fdcee5c7cd28073e117396f
ScalpelRed <Ongshlingbe@yandex.ru>
2025-01-05 02:30:03 +0800

    Small cleanup (trying to remember how to use git)

--- ConsoleCamera/Program.cs
+++ ConsoleCamera/Program.cs
@@ -1,18 +1,16 @@
 ﻿using Emgu.CV;
 
 class Program
-{
-
-    static VideoCapture Capture;
-
-    static readonly string[] grs =
+{ 
+    static readonly string[] Colors =
     {
         "  ", " ░", "░░", "░▒", "▒▒", "▒▓", "▓▓", "▓█", "██"
     };
 
-    static int stepX;
-    static int stepY;
-    static int interval;
+    static VideoCapture Capture;
+    static int StepX;
+    static int StepY;
+    static int TimeDelta;
 
     static void Main()
     {
@@ -25,44 +23,44 @@ class Program
         Console.Write("Quality X (from 0 to 1, empty for 0.25) ");
         try
         {
-            float d = float.Parse(Console.ReadLine().Replace('.', ','));
-            if (d <= 0 || d > 1) stepX = 4;
-            else stepX = (int)(1f / d);
+            float d = float.Parse(Console.ReadLine()!.Replace('.', ','));
+            if (d <= 0 || d > 1) StepX = 4;
+            else StepX = (int)(1f / d);
         }
         catch (FormatException)
         {
             Console.WriteLine("Invalid format, value was set to 0.25");
-            stepX = 4;
+            StepX = 4;
         }
         Console.Write("Quality Y (from 0 to 1, empty for 0.25) ");
         try
         {
-            float d = float.Parse(Console.ReadLine().Replace('.', ','));
-            if (d <= 0 || d > 1) stepY = 4;
-            else stepY = (int)(1f / d);
+            float d = float.Parse(Console.ReadLine()!.Replace('.', ','));
+            if (d <= 0 || d > 1) StepY = 4;
+            else StepY = (int)(1f / d);
         }
         catch (FormatException)
         {
             Console.WriteLine("Invalid format, value was set to 0.25");
-            stepY = 4;
+            StepY = 4;
         }
         Console.Write("Target fps (values bigger than 20 are not recommended) ");
         try
         {
-            float d = float.Parse(Console.ReadLine());
-            if (d <= 0) interval = 500;
-            else interval = (int)(1000 / d);
+            float d = float.Parse(Console.ReadLine()!);
+            if (d <= 0) TimeDelta = 500;
+            else TimeDelta = (int)(1000 / d);
         }
         catch (FormatException)
         {
             Console.WriteLine("Invalid format, value was set to 2");
-            interval = 500;
+            TimeDelta = 500;
         }
 
         while (true)
         {
             ProcessFrame();
-            Thread.Sleep(interval);
+            Thread.Sleep(TimeDelta);
         }
     }
 
@@ -81,7 +79,7 @@ class Program
                 Console.Clear();
                 clearCounter = 0;
             }
-            Console.WriteLine(FrameToString(stepX, stepY));
+            Console.WriteLine(FrameToString(StepX, StepY));
         }
 
         string FrameToString(int wx, int wy)
@@ -92,7 +90,7 @@ class Program
                 for (int p = frame.Cols - 1; p > 0; p -= wx)
                 {
                     int pos = (p + i * frame.Cols) * 3;
-                    res += grs[(int)(dat[pos + 2] / 255f * 8)];
+                    res += Colors[(int)(dat[pos + 2] / 255f * 8)];
 
                 }
                 res += "\n";
